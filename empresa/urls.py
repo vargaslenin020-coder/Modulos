e33d772b0ebc4fd839080592a0f1477147cb24e8
adrianquilumba7-boop <adrianquilumba7@gmail.com>
2025-10-24 08:37:27 -0500

    vr

--- empresa/urls.py
+++ empresa/urls.py
@@ -6,8 +6,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # SOLO UNA de estas líneas - ELIMINA LA OTRA
+
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile/', views.perfil, name='profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
